37d2fb36adda43a6719ccf5f83e987af71f7574c
Vaibhavugile <vaibhavugile7@gmail.com>
2026-05-08 15:15:03 +0530

    updatediosbuildsetig

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,59 @@
-import Flutter
 import UIKit
+import Flutter
+import Firebase
+import FirebaseMessaging
+import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // 🔥 Firebase Init
+    FirebaseApp.configure()
+
+    // 🔥 Notification Permission
+    UNUserNotificationCenter.current().delegate = self
+
+    let authOptions: UNAuthorizationOptions = [
+      .alert,
+      .badge,
+      .sound
+    ]
+
+    UNUserNotificationCenter.current().requestAuthorization(
+      options: authOptions,
+      completionHandler: { _, _ in }
+    )
+
+    application.registerForRemoteNotifications()
+
+    // 🔥 FCM Delegate
+    Messaging.messaging().delegate = self
+
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+    return super.application(
+      application,
+      didFinishLaunchingWithOptions: launchOptions
+    )
+  }
+
+  // 🔥 FCM TOKEN
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("FCM TOKEN: \(fcmToken ?? "")")
+  }
+
+  // 🔥 Foreground Notifications
+  override func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    willPresent notification: UNNotification,
+    withCompletionHandler completionHandler:
+      @escaping (UNNotificationPresentationOptions) -> Void
+  ) {
+    completionHandler([.banner, .sound, .badge])
   }
 }
